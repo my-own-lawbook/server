4fbd182a9cf1bba17a893f9101b4a13ce1ec7cc2
simon <simon@bumiller.me>
2024-09-08 16:11:05 +0200

    Revert "Added data conversion for UUID"
    This reverts commit 673fa536f3050d40c747f876b3640cee9f55bd8a.

--- rest/src/main/kotlin/me/bumiller/mol/rest/plugins/DataConversion.kt
+++ rest/src/main/kotlin/me/bumiller/mol/rest/plugins/DataConversion.kt
@@ -4,8 +4,6 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.dataconversion.*
 import kotlinx.datetime.LocalDate
 import me.bumiller.mol.model.Gender
-import me.bumiller.mol.model.http.badFormat
-import java.util.UUID
 
 /**
  * Installs the [DataConversion] plugin into the application
@@ -14,7 +12,6 @@ internal fun Application.dataConversion() {
     install(DataConversion) {
         localDate()
         gender()
-        uuid()
     }
 }
 
@@ -23,11 +20,7 @@ private typealias Config = io.ktor.util.converters.DataConversion.Configuration
 private fun Config.localDate() {
     convert<LocalDate> {
         decode { values ->
-            try {
-                LocalDate.parse(values.single())
-            } catch (e: Exception) {
-                badFormat("date", values.single())
-            }
+            LocalDate.parse(values.single())
         }
 
         encode { date ->
@@ -44,27 +37,12 @@ private fun Config.gender() {
                 "female" -> Gender.Female
                 "other" -> Gender.Other
                 "disclosed" -> Gender.Disclosed
-                else -> badFormat("gender", values.single())
+                else -> throw IllegalStateException("Did not find a matching gender for string '${values.single()}'")
             }
         }
 
         encode { gender ->
             listOf(gender.serializedName)
-        }
-    }
-}
-
-private fun Config.uuid() {
-    convert<UUID> {
-        decode { values ->
-            try {
-                UUID.fromString(values.single())
-            } catch (e: Exception) {
-                badFormat("uuid", values.single())
-            }
-        }
-        encode { uuid ->
-            listOf(uuid.toString())
         }
     }
 }
