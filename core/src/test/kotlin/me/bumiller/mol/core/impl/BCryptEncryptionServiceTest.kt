028dce232f9d767b2d8e95ad0d27730d10102cc5
simon <simon@bumiller.me>
2024-09-13 22:32:38 +0200

    MOL-17: Added tests for BCryptEncryptionService

---
+++ core/src/test/kotlin/me/bumiller/mol/core/impl/BCryptEncryptionServiceTest.kt
@@ -0,0 +1,26 @@
+package me.bumiller.mol.core.impl
+
+import me.bumiller.mol.core.EncryptionService
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class BCryptEncryptionServiceTest {
+
+    val encryptionService: EncryptionService = BCryptEncryptionService()
+
+    @Test
+    fun `encryption and decryption works`() {
+        val password = "JD§(Q)JD=)"
+
+        val hash = encryptionService.encrypt(password)
+        val verified1 = encryptionService.verify("ddq98329", hash)
+        val verified2 = encryptionService.verify("ao0UDJQ=)dq0D", hash)
+        val verified3 = encryptionService.verify(password, hash)
+
+        assertFalse(verified1)
+        assertFalse(verified2)
+        assertTrue(verified3)
+    }
+
+}
