b8bf10419e441c9c9710f7094d73e4af5dfea44e
simon <simon@bumiller.me>
2024-09-15 17:15:16 +0200

    Added note to OptionalSerializer

--- common/src/main/kotlin/me/bumiller/mol/common/serialization/OptionalSerializer.kt
+++ common/src/main/kotlin/me/bumiller/mol/common/serialization/OptionalSerializer.kt
@@ -2,22 +2,22 @@ package me.bumiller.mol.common.serialization
 
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerializationException
-import kotlinx.serialization.Serializer
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.JsonDecoder
 import me.bumiller.mol.common.Optional
 import me.bumiller.mol.common.present
 
 /**
  * Serializer for the custom [Optional] box type.
  *
+ * Problem: This serializer only works as expected when **deserializing**. This is acceptable, as we usually only get optionals from REST-requests, and do not respond with optionals. This however makes testing hard. TODO: In the future, this should be fixed.
+ *
  * See also: [Blog entry by Alex Vanyo]([https://livefront.com/writing/kotlinx-serialization-de-serializing-jsons-nullable-optional-properties/])
  *
  * @param T The type inside the optional
  * @param elementSerializer The serializer for [T]
  */
-class OptionalSerializer<T>(val elementSerializer: KSerializer<T>) : KSerializer<Optional<T>> {
+class OptionalSerializer<T>(private val elementSerializer: KSerializer<T>) : KSerializer<Optional<T>> {
 
     override val descriptor = elementSerializer.descriptor
 
