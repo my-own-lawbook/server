ff94f0fd29dace957520458b3561d6f3eccd72ae
simon <simon@bumiller.me>
2024-09-20 22:15:31 +0200

    MOL-62: Added tests for DatabaseInvitationContentService

--- core/src/test/kotlin/me/bumiller/mol/test/util/EntityGenerating.kt
+++ core/src/test/kotlin/me/bumiller/mol/test/util/EntityGenerating.kt
@@ -1,6 +1,7 @@
 package me.bumiller.mol.test.util
 
 import kotlinx.datetime.DatePeriod
+import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.plus
 import me.bumiller.mol.database.table.*
@@ -60,3 +61,30 @@ fun lawSectionEntity(key: Long) = LawSection.Model(
 )
 
 fun lawSectionEntities(count: Long) = (1L..count).map(::lawSectionEntity)
+
+fun roleString(key: Long) =
+    if (key % 2 == 0L) "admin"
+    else if (key % 3 == 0L) "write"
+    else if (key % 5 == 0L) "update"
+    else "read"
+
+fun invitationStatusEntity(key: Long) =
+    if (key % 2 == 0L) BookInvitation.Status.Open
+    else if (key % 3 == 0L) BookInvitation.Status.Denied
+    else if (key % 5 == 0L) BookInvitation.Status.Revoked
+    else BookInvitation.Status.Accepted
+
+fun invitationEntity(key: Long) = BookInvitation.Model(
+    key,
+    userEntity(key),
+    lawBookEntity(key),
+    userEntity(key),
+    roleString(key),
+    Instant.fromEpochSeconds(key * 1000),
+    null,
+    invitationStatusEntity(key),
+    null,
+    null
+)
+
+fun invitationEntities(count: Long) = (1L..count).map(::invitationEntity)
