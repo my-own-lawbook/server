0ef10c3fb2ce59b44055498dece30c42ebbc1e01
simon <simon@bumiller.me>
2024-09-20 11:06:29 +0200

    MOL-54: Deleted old AccessValidator methods

--- validation/src/main/kotlin/me/bumiller/mol/validation/AccessValidator.kt
+++ validation/src/main/kotlin/me/bumiller/mol/validation/AccessValidator.kt
@@ -1,6 +1,5 @@
 package me.bumiller.mol.validation
 
-import me.bumiller.mol.model.User
 import me.bumiller.mol.model.http.RequestException
 
 /**
@@ -71,65 +70,5 @@ interface AccessValidator {
         userId: Long,
         throwOnRestricted: Boolean = true
     ): Boolean?
-
-    /**
-     * Validates whether a user has read access to a book, i.e. whether a user is either creator or member of a book.
-     *
-     * Will throw a [RequestException] with 404 if the access is not available
-     *
-     * @param user The user
-     * @param bookId The id of the book
-     */
-    suspend fun validateReadBook(user: User, bookId: Long)
-
-    /**
-     * Validates whether a user has read access to an entry, i.e. whether a user is either creator or member of the parent book.
-     *
-     * Will throw a [RequestException] with 404 if the access is not available
-     *
-     * @param user The user
-     * @param entryId The id of the entry
-     */
-    suspend fun validateReadEntry(user: User, entryId: Long)
-
-    /**
-     * Validates whether a user has read access to a section, i.e. whether a user is either creator or member of the parent book.
-     *
-     * Will throw a [RequestException] with 404 if the access is not available
-     *
-     * @param user The user
-     * @param sectionId The id of the entry
-     */
-    suspend fun validateReadSection(user: User, sectionId: Long)
-
-    /**
-     * Validates whether a user has write access to a book, i.e. whether a user is the creator of the book.
-     *
-     * Will throw a [RequestException] with 404 if the access is not available
-     *
-     * @param user The user
-     * @param bookId The id of the entry
-     */
-    suspend fun validateWriteBook(user: User, bookId: Long)
-
-    /**
-     * Validates whether a user has write access to an entry, i.e. whether a user is the creator of the parent book.
-     *
-     * Will throw a [RequestException] with 404 if the access is not available
-     *
-     * @param user The user
-     * @param entryId The id of the entry
-     */
-    suspend fun validateWriteEntry(user: User, entryId: Long)
-
-    /**
-     * Validates whether a user has write access to a section, i.e. whether a user is the creator of the parent book.
-     *
-     * Will throw a [RequestException] with 404 if the access is not available
-     *
-     * @param user The user
-     * @param sectionId The id of the section
-     */
-    suspend fun validateWriteSection(user: User, sectionId: Long)
 
 }
--- validation/src/main/kotlin/me/bumiller/mol/validation/impl/ServiceAccessValidator.kt
+++ validation/src/main/kotlin/me/bumiller/mol/validation/impl/ServiceAccessValidator.kt
@@ -6,10 +6,8 @@ import me.bumiller.mol.core.data.UserService
 import me.bumiller.mol.core.exception.ServiceException
 import me.bumiller.mol.model.LawBook
 import me.bumiller.mol.model.MemberRole
-import me.bumiller.mol.model.User
 import me.bumiller.mol.model.http.RequestException
 import me.bumiller.mol.model.http.internal
-import me.bumiller.mol.model.http.notFoundIdentifier
 import me.bumiller.mol.validation.AccessValidator
 import me.bumiller.mol.validation.LawPermission
 import me.bumiller.mol.validation.LawResourceScope
@@ -93,75 +91,6 @@ internal class ServiceAccessValidator(
         }
     } catch (e: ServiceException) {
         null
-    }
-
-    override suspend fun validateReadBook(user: User, bookId: Long) {
-        bookAccessLevel(user.id, bookId).requireRead("law-book", bookId.toString())
-    }
-
-    override suspend fun validateReadEntry(user: User, entryId: Long) {
-        entryAccessLevel(user.id, entryId).requireRead("law-entry", entryId.toString())
-    }
-
-    override suspend fun validateReadSection(user: User, sectionId: Long) {
-        sectionAccessLevel(user.id, sectionId).requireRead("law-section", sectionId.toString())
-    }
-
-    override suspend fun validateWriteBook(user: User, bookId: Long) {
-        bookAccessLevel(user.id, bookId).requireWrite("law-book", bookId.toString())
-    }
-
-    override suspend fun validateWriteEntry(user: User, entryId: Long) {
-        entryAccessLevel(user.id, entryId).requireWrite("law-entry", entryId.toString())
-    }
-
-    override suspend fun validateWriteSection(user: User, sectionId: Long) {
-        sectionAccessLevel(user.id, sectionId).requireWrite("law-section", sectionId.toString())
-    }
-
-
-    // True for write, false for read, null for not at all
-    private suspend fun bookAccessLevel(userId: Long, bookId: Long): Boolean? {
-        val book = try {
-            lawContentService.getSpecificBook(id = bookId)
-        } catch (e: ServiceException) {
-            null
-        }
-
-        val isCreator = book?.creator?.id == userId
-        val isMember = userId in (book?.members?.map(User::id) ?: emptyList())
-
-        return if (isCreator) true
-        else if (isMember) false
-        else null
-    }
-
-    private suspend fun entryAccessLevel(userId: Long, entryId: Long): Boolean? {
-        val book = try {
-            lawContentService.getBookByEntry(entryId)
-        } catch (e: ServiceException) {
-            null
-        }
-
-        return book?.id?.let { bookAccessLevel(userId, it) }
-    }
-
-    private suspend fun sectionAccessLevel(userId: Long, sectionId: Long): Boolean? {
-        val entry = try {
-            lawContentService.getEntryForSection(sectionId)
-        } catch (e: ServiceException) {
-            null
-        }
-
-        return entry?.id?.let { entryAccessLevel(userId, it) }
-    }
-
-    private fun Boolean?.requireRead(resourceName: String, identifier: String) {
-        if (this == null) notFoundIdentifier(resourceName, identifier)
-    }
-
-    private fun Boolean?.requireWrite(resourceName: String, identifier: String) {
-        if (this == null || !this) notFoundIdentifier(resourceName, identifier)
     }
 
 }
