a3043e566f985483bdd7ae4e2e2bc148583b39a0
simon <simon@bumiller.me>
2024-09-21 20:25:13 +0200

    MOL-66: ScopedPermission model to model permission of a user

---
+++ model/src/main/kotlin/me/bumiller/mol/model/ScopedPermission.kt
@@ -0,0 +1,192 @@
+package me.bumiller.mol.model
+
+/**
+ * Denotes the permission for a specific action (or scope of actions) that a user can be granted or not.
+ *
+ * Is scoped to a specific resource, like for example "the book with id 4".
+ */
+sealed class ScopedPermission(
+
+    /**
+     * The name of the resource
+     */
+    val resourceName: String,
+
+    /**
+     * The id of the specific resource the permission may be bound to
+     */
+    val id: Long
+
+) {
+
+    /**
+     * Permissions related to users
+     */
+    sealed class Users(id: Long) : ScopedPermission("user", id) {
+
+        /**
+         * Permission to read a specific user.
+         *
+         * @param id The id of the user.
+         * @param profile Whether access to the users profile is included
+         * @param email Whether access to the users email is included
+         */
+        class Read(val profile: Boolean = false, val email: Boolean = false, id: Long) : Users(id)
+
+    }
+
+    /**
+     * Permissions related to law-sections
+     */
+    sealed class Sections(id: Long) : ScopedPermission("law-section", id) {
+
+        /**
+         * Permission to read a specific section.
+         *
+         * @param id The id of the section.
+         */
+        class Read(id: Long) : Sections(id)
+
+        /**
+         * Permission to write to a specific section, i.e. change attributes and delete it.
+         *
+         * @param id The id of the section.
+         */
+        class Write(id: Long) : Sections(id)
+
+    }
+
+    /**
+     * Permissions related to law-entries
+     */
+    sealed class Entries(id: Long) : ScopedPermission("law-entry", id) {
+
+        /**
+         * Permission to read a specific entry.
+         *
+         * @param id The id of the entry.
+         */
+        class Read(id: Long) : Entries(id)
+
+        /**
+         * Permission to write to a specific entry, i.e. change attributes and delete it.
+         *
+         * @param id The id of the entry.
+         */
+        class Write(id: Long) : Entries(id)
+
+        /**
+         * Permissions related to children of law-entries
+         */
+        sealed class Children(id: Long) : Entries(id) {
+
+            /**
+             * Permission to read all children of the book
+             *
+             * @param id The id of the book
+             */
+            class Read(id: Long) : Children(id)
+
+            /**
+             * Permission to update the contents and attributes of all children of a book
+             *
+             * @param id The id of the book
+             */
+            class Update(id: Long) : Children(id)
+
+            /**
+             * Permission to manage the children of a book, i.e. create new ones
+             *
+             * @param id The id of the book
+             */
+            class Create(id: Long) : Children(id)
+
+        }
+
+    }
+
+    /**
+     * Permissions related to law-books
+     */
+    sealed class Books(id: Long) : ScopedPermission("law-book", id) {
+
+        /**
+         * Permission to read a specific book.
+         *
+         * @param id The id of the book.
+         */
+        class Read(id: Long) : Books(id)
+
+        /**
+         * Permission to write to a specific book, i.e. change attributes and delete it.
+         *
+         * @param id The id of the book.
+         */
+        class Write(id: Long) : Books(id)
+
+        /**
+         * Permissions related to all children of a book
+         */
+        sealed class Children(id: Long) : Books(id) {
+
+            /**
+             * Permission to read all children of the book
+             *
+             * @param id The id of the book
+             */
+            class Read(id: Long) : Children(id)
+
+            /**
+             * Permission to update the contents and attributes of all children of a book
+             *
+             * @param id The id of the book
+             */
+            class Update(id: Long) : Children(id)
+
+            /**
+             * Permission to manage the children of a book, i.e. create new ones
+             *
+             * @param id The id of the book
+             */
+            class Create(id: Long) : Children(id)
+
+        }
+
+        /**
+         * Permissions related to the members of a book
+         */
+        sealed class Members(id: Long) : Books(id) {
+
+            /**
+             * Permission to read the members of the book
+             *
+             * @param id The id of the book
+             */
+            class Read(id: Long) : Members(id)
+
+            /**
+             * Permission to remove a member from a book
+             *
+             * @param id The id of the book
+             */
+            class Remove(id: Long) : Members(id)
+
+            /**
+             * Permission to view the invitations of the book
+             *
+             * @param id The id of the book
+             */
+            class ReadInvitations(id: Long) : Members(id)
+
+            /**
+             * Permission to manage the invitations of the book, i.e. create new ones and revoke other ones
+             *
+             * @param id The id of the book
+             */
+            class ManageInvitations(id: Long) : Members(id)
+
+        }
+
+    }
+
+}
